e6ded37ad45fd2f54c021f6251244b357615b470
Tim Mueller <trmueller5@gmail.com>
2023-06-12 13:54:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return `This one is on me!`;
+  } else if (distance > 400 && distance <= 2000) {
+    return `That will be twenty bucks.`;
+  } else if (distance > 2000 && distance < 2500){
+    return `I will gladly take your thirty bucks.`;
+  } else if (distance > 2500) {
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const message = city === "NYC" ? `Ok, sounds good.` : `No go.`;
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case `generous` : return `Thank you so much.`;
+    break;
+    case `not as generous` : return 'Thank you.';
+    break;
+    default: return `Bye.`;
+  }
 }
